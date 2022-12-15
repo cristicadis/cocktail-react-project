6389e1f50ee0c5061cd1e12690e62566aed5a689
Cristian Cadis <cristiancadis@Cristians-Air.fritz.box>
2022-12-15 14:14:02 +0100

    fitrst

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+   <section className='section about-section'>
+     <h1 className='section-title'>about us</h1>
+     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi beatae, quia aperiam ex non repudiandae doloremque laborum eius sapiente quibusdam, libero consequuntur dignissimos eveniet cum. Vitae, quis accusamus, ullam corporis dolores similique architecto magni assumenda quam sint praesentium magnam asperiores.</p>
+   </section>
+  )
+}
+
+export default About
